e9289165397a6fa45f09426d3cbaeaa4b093f4ca
Navneet <46026280+navghosh@users.noreply.github.com>
2021-06-24 11:16:12 +0530

    hellonavneet.py

---
+++ hellonavneet.py
@@ -0,0 +1,12 @@
+def lambda_handler(event, context):
+    print("In lambda handler")
+    
+    resp = {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": "Hello Navneet, Welcome to Lambda Demo project"
+    }
+    
+    return resp
